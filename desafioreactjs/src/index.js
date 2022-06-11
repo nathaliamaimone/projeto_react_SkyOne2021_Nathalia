d9f3eee35461684c39919c8aafc51297573532e0
nathaliamaimone <nathalia.pmaimone@gmail.com>
2022-06-11 17:25:14 -0300

    inspecionando código

--- desafioreactjs/src/index.js
+++ desafioreactjs/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends ReadableStreamDefaultController.Component {
+class Square extends React.Component {
     render() {
         return(
             <button className="square">
-                {/* TODO */}
+                {this.props.value}
             </button>
         );
     }
 }
 class Board extends React.Component {
     renderSquare (i) {
-        return <Square />;
+        return <Square value={i} />;
     }
     render() {
         const status = 'Next player: X';
